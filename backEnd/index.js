0cbeaedaa88e913dc0962b570acfe5decd9793dd
marcelaobeso <march.obeso@gmail.com>
2022-01-06 21:21:01 -0600

    more back

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,7 +2,20 @@ import express from "express";
 import bodyParser from "body-parser"
 
 let lastId = 0;
-const productos = [];
+let productos = [
+    {
+        codigo: 1, 
+        nombre: "producto 1", 
+        precio: 10, 
+        cantidad: 100
+    },
+    {
+        codigo: 2, 
+        nombre: "producto 2", 
+        precio: 50, 
+        cantidad: 200
+    }
+];
 
 const app = express();
 
@@ -20,6 +33,18 @@ app.post("/productos", (req,res)=>{
     res.json(producto)
 })
 
+app.put("/productos/:codigo", (req, res) => {
+    const codigo = parseInt(req.params.codigo, 10);
+    const producto = productos.find(p => p.codigo == codigo);
+    if(!producto){
+        res.status(404);
+        res.json({mensaje: "no hay"});
+    } else {
+        const index = productos.indexOf(productos);
+        const newProduct = productos[index]
+    }
+})
+
 app.listen(5000, () => {
     console.log("servidor escuchando en puerto 5000");
 });
@@ -29,3 +54,4 @@ function logs (req, res, next)
     console.log(`${req.method}: ${req.originalUrl}`);
     next();
 }
+
